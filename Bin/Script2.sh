68f27ee37d700c2b36bf810f749a74d4b2b5c1b5
CEEP10101991 <59848265+CEEP10101991@users.noreply.github.com>
2020-05-29 14:51:20 -0500

    Update Script2.sh

--- Bin/Script2.sh
+++ Bin/Script2.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-### crea un directorio output dentro de /tests.denovo, para las varables seleccionadas (n:1,2,3)
+#This script perform a prove for an small data set (previously selected three samples),this exercise may be useful to make the pertinent variable de-novo variables selection.
+#For this specific exercise we will use the "n" values (1-3) as the tested variable
+
+### To create the output directories at /tests.denovo, for each one of the selected variables (n:1,2,3)
 
 n_values="1 2 3 "
 for n in $n_values ;do
 	mkdir -p ../tests.denovo/stacks.n$n
 done
-### Corre el wrapper denovo_map de stacks
-
- ## variables para loop
- n_values="1 2 3"
- popmap=../info/popmap_TAREA.tsv
-
- ## utilizar loop para construir los flags de `denovo_map`
+### To run the wrapper to generate the denovo_map
+ ## variables for the loop
+ 
+ n_values="1 2 3"#to set the n values
+ popmap=../info/popmap_TAREA.tsv #to set the population map necesary to perform the wrapper 
+ 
  for n in $n_values ;do
  M=2
  m=3
@@ -22,3 +24,9 @@ done
  	log_file=$out_dir/denovo_map.oe
  	denovo_map.pl --samples $reads_dir -O $popmap -o $out_dir -M $M -n $n -m $m
  done
+ #samples=sepecify the location of ower previously demultipleded data
+ #O=et the location of the population map necesary to perform the wrapper
+ #o=pecify the output directory 
+ #n=ber of mismatches allowed between stacks between individuals (for cstacks).
+ #M=number of mismatches allowed between stacks within individuals (for ustacks)
+ #m=mnimum stack depth / minimum depth of coverage (for ustacks)
