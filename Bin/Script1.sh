146c9455998adf5a31e65ed5b70cf5d2ecb84fd2
CEEP10101991 <59848265+CEEP10101991@users.noreply.github.com>
2020-05-29 14:21:18 -0500

    Update Script1.sh

--- Bin/Script1.sh
+++ Bin/Script1.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# limpia y demultiplexea las muesrtras especificadas en ../info/barco$
+# Before running the excersise, to continue with the original files structure of this excerseise the usuary must generate three new foldears that will be container of the diferent outputs of this protocol
+mkdir ../cleaned
+mkdir ../stacks.denovo
+mkdir ../tests.denovo
+
+#To clean and demultiplex the selected salmples un the barcode file (for this excersise single-end, Illumina HiSeq data)
+
+raw_dir=../raw/lane1/ #specifys the directory containing the input files
+barcodes_file=../info/barcodes.lane1.tsv #specifys file containing my barcodes
+out_dir=../cleaned/#specifys the folder to enter the output files
+
+process_radtags -p $raw_dir -b $barcodes_file -o $out_dir -e sbfI --i$ #-e provide the restriction enzyme used (cut site occurs on single-end read)
 
 
-raw_dir=../raw/lane1/
-barcodes_file=../info/barcodes.lane1_TAREA.tsv
-out_dir=../cleaned/
-
-process_radtags -p $raw_dir -b $barcodes_file -o $out_dir -e sbfI --i$
 
 
 
