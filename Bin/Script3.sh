786f1990cab2a9c3d66b05418b04289f0a7062c8
CEEP10101991 <59848265+CEEP10101991@users.noreply.github.com>
2020-05-29 15:33:00 -0500

    Update Script3.sh

--- Bin/Script3.sh
+++ Bin/Script3.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-## Exporta loci a analizar con populations
+## To export output from Script2.sh to perfomr an analysis in "populations"
 
-## variables para loop
+## Loop varables
  n_values="1 2 3"
+ ## loop
  for n in $n_values ;do
  	stacks_dir=../tests.denovo/stacks.n$n
  	out_dir=$stacks_dir/populations.r80
@@ -12,5 +13,7 @@
  	populations -P $stacks_dir -O $out_dir -r 0.80 --plink
  done
 
-
-
+#P=input directory
+#O= output directory
+#r= at 0.80 filter loci that are only in at least 80% of the samples
+#--plink= to export into plink format file for future analysis 
